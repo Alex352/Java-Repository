7bb73e0f8b81a7c2466aa1e1da6284752eea0245
Alex352 <alex.mcclure352@gmail.com>
2017-03-31 13:11:01 -0500

    update

---
+++ Chapter2/src/gameZone/MadLibs.java
@@ -0,0 +1,57 @@
+package gameZone;
+
+import java.util.Scanner;
+
+public class MadLibs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String color;
+		String wordEST;
+		String bodyPartPlural;
+		String animal; 
+		String noun;
+		String pluralNoun; 
+		int a, b, c;
+		System.out.println("Welcome my MadLibs game.");
+		
+		Scanner inputDevice = new Scanner(System.in);
+		
+		System.out.println("Enter a color>>");
+		color = inputDevice.nextLine(); 
+		
+		
+		System.out.println("Enter a word ending in EST>>");
+		 wordEST= inputDevice.nextLine();
+		
+		System.out.println("Enter a body part plural>>");
+		bodyPartPlural = inputDevice.nextLine(); 
+		
+		
+		System.out.println("Enter an animal>>");
+		animal = inputDevice.nextLine(); 
+		
+		
+		System.out.println("Enter a noun>>");
+		noun = inputDevice.nextLine(); 
+		
+		
+		System.out.println("Enter a plural noun>>");
+		pluralNoun= inputDevice.nextLine(); 
+		
+		System.out.println("Enter a number >>> ");
+		a = inputDevice.nextInt();
+		inputDevice.nextLine();
+		System.out.println("Enter another number less than the previous>>> ");
+		b = inputDevice.nextInt();
+		inputDevice.nextLine();
+	c = a -b; 
+		System.out.println("The " + color + " Dragon is the " + wordEST + " Dragon of all. It has " + c + 
+				bodyPartPlural + " , and an " + animal + "shaped like a " + noun + 
+				". It loves to eat " + pluralNoun + ", although it will feast on nearly anything.");
+		
+		
+	
+	}
+
+}
