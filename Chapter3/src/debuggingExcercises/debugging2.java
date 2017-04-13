a37b11b4823c78f520704eda6e634b6c2565c6e2
Alex352 <alex.mcclure352@gmail.com>
2017-04-13 12:04:06 -0500

    update

--- Chapter3/src/debuggingExcercises/debugging2.java
+++ Chapter3/src/debuggingExcercises/debugging2.java
@@ -4,7 +4,22 @@ public class debugging2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		double a = 2;
+		double b = 5; 
+		double c = 10;
+	     	add(a, b);
+	     	add(b, c);
+	     	subtract(c, a);            
+	   }
+	   public static void add(double b, double  c)
+	   {
+	      System.out.println("The sum of " + b +
+	         " and " + c + " is " + (b + c));
+	   }
+	   public static void subtract(double c, double a)
+	   {
+	      System.out.println("The difference between " +
+	        c + " and " + a + " is " +  (c - a));
 	}
 
 }
