0b4e7d2134af1fca8fb05a348b50b1d6b622c24a
Alex352 <alex.mcclure352@gmail.com>
2017-04-21 13:09:50 -0500

    update

--- Chapter5/src/excersises/EvenOdd.java
+++ Chapter5/src/excersises/EvenOdd.java
@@ -1,10 +1,25 @@
 package excersises;
-
+import javax.swing.JOptionPane;
 public class EvenOdd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String numStr, result;
+	      int num, choice;
 
+	      do 
+	      {
+	         numStr = JOptionPane.showInputDialog ("Enter an integer: ");
+
+	         num = Integer.parseInt(numStr);
+
+	         result = "That number is " + ((num%2 == 0) ? "even" : "odd");
+
+	         JOptionPane.showMessageDialog (null, result);
+
+	         choice = JOptionPane.showConfirmDialog (null, "Do Another?");
+	      }
+	      while (choice == JOptionPane.YES_OPTION);
 	}
 
 }
