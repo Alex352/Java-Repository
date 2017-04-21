2848e1eb04ec20cf7cfe7b44beb88175bae3b4d7
Alex352 <alex.mcclure352@gmail.com>
2017-04-21 13:20:11 -0500

    update

--- Chapter5/src/excersises/AscendingAndDescending.java
+++ Chapter5/src/excersises/AscendingAndDescending.java
@@ -2,7 +2,8 @@ package excersises;
 
 import java.util.Scanner;
 
-public class AscendingAndDescending {
+public class AscendingAndDescending 
+{
 
 	public static void main(String[] args) 
 	{
@@ -16,41 +17,38 @@ public class AscendingAndDescending {
 		System.out.println("What's your third number? >> ");
 		num3 = input.nextInt();
 		
-		if(num1> num2 & num1 >num3)
-		{
-			if(num2>num3)
-				System.out.println(num1+ " > " + num2 + " > " + num3);
-		}
-			else if(num3>num2)
-		{
-				System.out.println(num3 + " > " + num2 );
-		}
+			if(num1> num2 & num1 >num3)
+			{
+				if(num2>num3)
+				{
+					System.out.println(num1+ " > " + num2 + " > " + num3);
+				}
+				else if(num3>num2)
+				{
+						System.out.println(num1+ " > "+ num3 + " > " + num2 );
+				}
+			}
+			
 		
-		if(num2>num1 & num2>num3)
-		{
-				System.out.println(num2+ " > " + num1 + " > " + num3);
-		}
-			if(num1>num3)
-		{
-				System.out.println(num1+ " > " + num3);	
-		}		
-			else if(num3>num1)
-		{
-				System.out.println(num3+ " > " + num1);	
-		{
-		if(num3>num1 & num3>num2);	
-		}
-				System.out.println(num3+ " > " + num2 + " > " + num1);
-		{
-			if(num1>num2);
-		}
-			System.out.println(num1+ " > " + num2 + " > " + num3);
-		{
-				if(num2>num1);
-		}	
-					System.out.println(num2+ " > " + num1);
-		{
+			if(num2>num1 & num2>num3)
+			{
+					
+				if(num1>num3)
+				{
+						System.out.println(num1+ " > " + num3);	
+				}		
+					else if(num3>num1)
+				{
+						System.out.println(num3+ " > " + num1);	
+				}
+			}
+		
+			if(num3>num1 & num3>num2);	
+			{
+					
+				
+				
+			}
 
+	}
 }
-}
-	}}
